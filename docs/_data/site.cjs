67117bf4ac6723568d740f3543267399051bd460
petecarapetyan <pete@datafundamentals.com>
2020-12-13 17:05:56 -0600

    initial

---
+++ docs/_data/site.cjs
@@ -0,0 +1,21 @@
+module.exports = function () {
+  return {
+    dir: 'ltr',
+    lang: 'en',
+    name: 'Rocket',
+    description: 'Rocket is the way to build fast static websites with a sprinkle of javascript',
+    socialLinks: [
+      {
+        name: 'GitHub',
+        url: 'https://github.com/dakmor/rocket',
+      },
+    ],
+    gitSiteUrl: 'https://github.com/dakmor/rocket',
+    helpUrl: 'https://github.com/daKmoR/rocket/issues',
+    logoAlt: 'Rocket Logo',
+    iconColorMaskIcon: '#3f93ce',
+    iconColorMsapplicationTileColor: '#1d3557',
+    iconColorThemeColor: '#1d3557',
+    socialMediaImage: '/_assets/social-media-image.jpg',
+  };
+};
---
+++ docs/index.md
@@ -0,0 +1,3 @@
+# webAppWriter.com
+
+Welcome
